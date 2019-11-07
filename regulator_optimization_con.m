c8a5b7dd593f47b76b4e2b72803062d35f84788c
Andreas Moltumyr <kallexm@hotmail.com>
2019-11-07 16:09:17 +0100

    Add help header section to regulator_optimization_con function.

--- regulator_optimization_con.m
+++ regulator_optimization_con.m
@@ -1,7 +1,15 @@
-%% FILE: regulator_optimization_con.m
-%% AUTHOR: Andreas Hanssen Moltumyr
-
 function [c, ceq] = regulator_optimization_con(x, G, reg_type, Np)
+    % REGULATOR_OPTIMIZATION_CON Function for calculating stability conditions
+    % with the assess_stability(...) function.
+    %
+    % [c, ceq] = regulator_optimization_con(x, G, reg_type, Np) - x are the control
+    % parameters to optimize, G is the plant transfer function (fotf object), reg_type
+    % is a regulator identifier that is used by the construct_system_fotfs(...) function
+    % in order to build the correct controller from the control parameters x. c_eq and c are the
+    % calculated values of the equality and inequality constraints, respectively.
+    % 
+    % AUTHOR: Andreas Hanssen Moltumyr
+
     %% Construct open-loop system transfer function
     L = construct_system_fotfs(G, reg_type, x);
     
@@ -31,12 +39,10 @@ function [c, ceq] = regulator_optimization_con(x, G, reg_type, Np)
     %% Evaluate Stability (Nyquist stability criterion approach)
     w_low  = 10^-10;
     w_high = 10^5;
-    [isStable,~,GMs_dB,~,~,~] = assess_stability(L,N_p,{w_low,w_high});
+    [~,~,GMs_dB,~,~,~] = assess_stability(L,N_p,{w_low,w_high});
     
     %% Calculate Equality Conditions (Stability)
-    c_eq1 = 100*(1 - isStable);
-    
-    ceq = [c_eq1];
+    ceq = [];
     
     %% Calculate Inequality Conditions (Stability)
     gain_margin_db = 6;
